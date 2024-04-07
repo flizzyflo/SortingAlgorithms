df801ac78a372a82a42c1ba6b3bd31cd511a7e38
flizzyflo <vip3r1@gmx.de>
2024-04-07 18:08:07 +0200

    updated visualisation

--- src/sortingAlgorithms/BubbleSort.py
+++ src/sortingAlgorithms/BubbleSort.py
@@ -1,8 +1,10 @@
-from typing import Callable
+from src.userInterface.colorenum import ColorEnum
+from src.userInterface.sortingcanvas import SortingCanvas
 
-def bubbleSort(values: list[int], createRectangles: Callable = None, finalizeSearch: Callable = None) -> None:
 
-    """Implementation of bubble sort. This sorting algorithm grabs a number an switches it with the next number, if the next one is less. The grabed number
+def bubbleSort(values: list[int], sortingCanvas: SortingCanvas) -> None:
+
+    """Implementation of bubble sort. This sorting algorithm grabs a number and switches it with the next number, if the next one is less. The grabed number
     "marches" through the list until a number bigger than itself is found. With this bigger number, the process repeats again Finally, the biggest number 
     is at the end of the array and the process is repeated until the whole array is sorted. This algorithm works in-place and is stable."""
 
@@ -20,19 +22,20 @@ def bubbleSort(values: list[int], createRectangles: Callable = None, finalizeSea
             if currentValue > values[currentIndex + 1]:  
                 # If next value is smaller than current value, swap both.
               
-                temporary_object: object = values[currentIndex + 1]
+                temporary_object: int = values[currentIndex + 1]
                 values[currentIndex + 1] = currentValue
                 values[currentIndex] = temporary_object
-               
-                if createRectangles:
+
+                if sortingCanvas:
+                    sortingCanvas.colorizeSingleBar(currentIndex, ColorEnum.GREEN.value)
+                    sortingCanvas.colorizeSingleBar(currentIndex + 1, ColorEnum.PURPLE.value)
                 # updates the GUI, according to the current values of the values list, if used in visualization.
-                    createRectangles(values)
+                    sortingCanvas.createRectangles(values)
                 
             else:
                 continue
 
-    if finalizeSearch:
+    if sortingCanvas:
     # changing the bar color to green when search is complete and algorithm is used in tkinter gui.        
-        for index in range(len(values)):
-            finalizeSearch(index)
+        sortingCanvas.endSearch(values)
            
