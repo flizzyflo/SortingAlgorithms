58fc9669b6fda51196a58592cc70ebd957c964f2
flizzyflo <vip3r1@gmx.de>
2024-04-07 18:11:18 +0200

    put window parameters into main

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+from src.userInterface.graphicaluserinterface import GraphicalUserInterface
 
-from UserInterface.GraphicalUserInterface import *
-from Settings.Settings import *
+ROOT_TITLE: str = "Search Algorithm Presentation"
+GEOMETRY_MEASUREMENT: str = "800x800"
+
 
 def main():
 
     window = GraphicalUserInterface()
- 
-    
+    window.title(ROOT_TITLE)
+    window.geometry(GEOMETRY_MEASUREMENT)
+
+    window.mainloop()
+
 
 if __name__ == "__main__":
     main()
