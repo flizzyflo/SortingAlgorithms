644d086ab37d0267a4adb7ebbec661b3e00e64b8
flizzyflo <vip3r1@gmx.de>
2024-04-07 18:20:15 +0200

    renamed file and move to new folder

--- src/numbergenerator/createrandomnumbers.py
+++ src/numbergenerator/createrandomnumbers.py
@@ -1,6 +1,8 @@
 from random import randint
+from typing import List
 
-def createRandomArray(desiredArraySize: int, seedStart: int = 100, seedEnd: int = 1_000_000) -> list[int]:
+
+def create_random_numbers(desiredArraySize: int, seedStart: int = 100, seedEnd: int = 1_000_000) -> List[int]:
 
     """This function creates an array containing 'desiredArraySize' integer within the range of 
     'seedStart' and 'seedEnd'."""
