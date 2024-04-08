e938c03261b5b0faf0a2af35dad984c016230cfb
flizzyflo <vip3r1@gmx.de>
2024-04-09 00:22:02 +0200

    renamed methods and added check for visuals

--- src/sortingAlgorithms/mergesort.py
+++ src/sortingAlgorithms/mergesort.py
@@ -16,26 +16,25 @@ class MergeSort(AbstractSort):
             self.sortingCanvas.endSearch(self.dataToSort)
 
     def sort(self):
-        self.mergesort(self.dataToSort, 0, len(self.dataToSort) - 1)
+        self.__mergesort(self.dataToSort, 0, len(self.dataToSort) - 1)
 
-    def mergesort(self, unsorted_array: List[int | float], leftIdx: int, rightIdx: int) -> None:
+    def __mergesort(self, unsorted_array: List[int | float], leftIdx: int, rightIdx: int) -> None:
 
         # more than one element unsorted array
         if leftIdx < rightIdx:
             midIndex = (leftIdx + rightIdx) // 2
 
             # division of every array until it contains just one element
-            self.mergesort(unsorted_array, leftIdx, midIndex)
-            self.mergesort(unsorted_array, midIndex + 1, rightIdx)
+            self.__mergesort(unsorted_array, leftIdx, midIndex)
+            self.__mergesort(unsorted_array, midIndex + 1, rightIdx)
 
             # merging elements together, but in place in original array
-            self.merge(unsorted_array, leftIdx, midIndex, rightIdx)
-            self.sortingCanvas.createRectangles(self.dataToSort)
+            self.__merge(unsorted_array, leftIdx, midIndex, rightIdx)
 
-    def merge(self, parentArray: List[int | float], leftIndex: int, middleIndex: int, rightIndex: int) -> None:
+
+    def __merge(self, parentArray: List[int | float], leftIndex: int, middleIndex: int, rightIndex: int) -> None:
 
         leftHalf = parentArray[leftIndex:middleIndex + 1]
-
 
         # middle index + 1 to get first element of right half from parent array
         # right index + 1 to include first element of right half from parent array since the rightindex + 1 is not picked (not inclusive)
@@ -48,40 +47,44 @@ class MergeSort(AbstractSort):
 
         # both pointers are within array bounds
         while leftHalfIdx < len(leftHalf) and rightHalfIdx < len(rightHalf):
-
-            self.sortingCanvas.colorizeSingleBar(parentArrayIdx, ColorEnum.GREEN.value)
+            if self.sortingCanvas:
+                self.sortingCanvas.colorizeSingleDrawnRectangle(parentArrayIdx, ColorEnum.GREEN.value)
 
             # swap left value to parent array index position move to next element in left list
             if leftHalf[leftHalfIdx] <= rightHalf[rightHalfIdx]:
                 parentArray[parentArrayIdx] = leftHalf[leftHalfIdx]
                 leftHalfIdx += 1
-                self.sortingCanvas.colorizeSingleBar(leftHalfIdx, ColorEnum.PURPLE.value)
+                if self.sortingCanvas:
+                    self.sortingCanvas.colorizeSingleDrawnRectangle(leftHalfIdx, ColorEnum.PURPLE.value)
 
             # swap right value to parent array index position move to next element in right list
             elif rightHalf[rightHalfIdx] < leftHalf[leftHalfIdx]:
                 parentArray[parentArrayIdx] = rightHalf[rightHalfIdx]
-                self.sortingCanvas.colorizeSingleBar(rightHalfIdx, ColorEnum.PURPLE.value)
                 rightHalfIdx += 1
+                if self.sortingCanvas:
+                    self.sortingCanvas.colorizeSingleDrawnRectangle(rightHalfIdx, ColorEnum.PURPLE.value)
 
             parentArrayIdx += 1
-
-            self.sortingCanvas.colorizeSingleBar(leftHalfIdx, ColorEnum.GREEN.value)
-            self.sortingCanvas.colorizeSingleBar(rightHalfIdx, ColorEnum.PURPLE.value)
-            self.sortingCanvas.createRectangles(self.dataToSort)
+            if self.sortingCanvas:
+                self.sortingCanvas.colorizeSingleDrawnRectangle(leftHalfIdx, ColorEnum.GREEN.value)
+                self.sortingCanvas.colorizeSingleDrawnRectangle(rightHalfIdx, ColorEnum.PURPLE.value)
+                self.sortingCanvas.drawRectanglesToCanvas(self.dataToSort)
 
         # append remainder of left list to parent array
         while leftHalfIdx < len(leftHalf):
             parentArray[parentArrayIdx] = leftHalf[leftHalfIdx]
             leftHalfIdx += 1
             parentArrayIdx += 1
-            self.sortingCanvas.colorizeSingleBar(leftHalfIdx, ColorEnum.GREEN.value)
 
-            self.sortingCanvas.createRectangles(self.dataToSort)
+            if self.sortingCanvas:
+                self.sortingCanvas.colorizeSingleDrawnRectangle(leftHalfIdx, ColorEnum.GREEN.value)
+                self.sortingCanvas.drawRectanglesToCanvas(self.dataToSort)
 
         # append remainder of right list to parent array
         while rightHalfIdx < len(rightHalf):
             parentArray[parentArrayIdx] = rightHalf[rightHalfIdx]
             rightHalfIdx += 1
             parentArrayIdx += 1
-            self.sortingCanvas.colorizeSingleBar(rightHalfIdx, ColorEnum.PURPLE.value)
-            self.sortingCanvas.createRectangles(self.dataToSort)
+            if self.sortingCanvas:
+                self.sortingCanvas.colorizeSingleDrawnRectangle(rightHalfIdx, ColorEnum.PURPLE.value)
+                self.sortingCanvas.drawRectanglesToCanvas(self.dataToSort)
