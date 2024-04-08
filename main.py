9daaa79748c6e0a508fd101c47e21732e337ce2e
flizzyflo <vip3r1@gmx.de>
2024-04-08 21:08:38 +0200

    added implemented sort algorithm list

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+from src.sortingAlgorithms.bogosort import BogoSort
+from src.sortingAlgorithms.bubblesort import BubbleSort
+from src.sortingAlgorithms.insertionsort import InsertionSort
+from src.sortingAlgorithms.mergesort import MergeSort
+from src.sortingAlgorithms.quicksort import QuickSort
+from src.sortingAlgorithms.radixsort import RadixSort
+from src.sortingAlgorithms.selectionsort import SelectionSort
 from src.userInterface.mainframe.graphicaluserinterface import GraphicalUserInterface
 
 ROOT_TITLE: str = "Search Algorithm Visualization"
-GEOMETRY_MEASUREMENT: str = "800x800"
+GEOMETRY_MEASUREMENT: str = "1400x800"
 
 
 def main():
+    sortingAlgorithms = [BubbleSort, InsertionSort, SelectionSort, MergeSort, RadixSort, QuickSort, BogoSort]
 
-    window = GraphicalUserInterface()
+    window = GraphicalUserInterface(sortingAlgorithms)
     window.title(ROOT_TITLE)
     window.geometry(GEOMETRY_MEASUREMENT)
 
