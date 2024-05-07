9ac389dee9cd71d5467d5e5d6a3616e4dd1e8087
flizzyflo <vip3r1@gmx.de>
2024-05-07 21:12:34 +0200

    added bucket colors for visuals

--- src/sortingAlgorithms/radixsort.py
+++ src/sortingAlgorithms/radixsort.py
@@ -10,6 +10,18 @@ class RadixSort(AbstractSort):
 
     def __init__(self, *, dataToSort: List[int], sortingCanvas: SortingCanvas):
         super().__init__(dataToSort=dataToSort, sortingCanvas=sortingCanvas)
+
+        self.bucketColors: dict[int, ColorEnum] = {0: ColorEnum.YELLOW.value,
+                                                   1: ColorEnum.PURPLE.value,
+                                                   2: ColorEnum.GREEN.value,
+                                                   3: ColorEnum.RED.value,
+                                                   4: ColorEnum.ORANGE.value,
+                                                   5: ColorEnum.GREEN.value,
+                                                   6: ColorEnum.LIGHTBLUE.value,
+                                                   7: ColorEnum.DARKBLUE.value,
+                                                   8: ColorEnum.BROWN.value,
+                                                   9: ColorEnum.BLACK.value}
+
         self.bucketContainer: dict[int, List[int]] = dict()
         self.maxNumberLength = len(str(max(self.dataToSort)))
         self.sort()
@@ -22,12 +34,15 @@ class RadixSort(AbstractSort):
         # as long as not first position is reached
         while numberPositionIndex >= 0:
 
-            for numberToSortIntoBucket in self.dataToSort:
+            for numberIndex, numberToSortIntoBucket in enumerate(self.dataToSort):
                 currentNumberAsString = str(numberToSortIntoBucket)
                 currentNumberAsString = currentNumberAsString.zfill(self.maxNumberLength)
 
                 bucketNumberAsString = currentNumberAsString[numberPositionIndex]
                 bucketNumber = int(bucketNumberAsString)
+
+                if self.sortingCanvas:
+                    self.sortingCanvas.colorizeSingleDrawnRectangle(numberIndex, self.bucketColors[bucketNumber])
 
                 self.addToBucket(bucketNumber, numberToSortIntoBucket)
 
@@ -38,6 +53,7 @@ class RadixSort(AbstractSort):
 
         self.putBucketsToBaseList()
         self.clearBuckets()
+
         if self.sortingCanvas:
             self.sortingCanvas.drawRectanglesToCanvas(self.dataToSort)
             self.sortingCanvas.endSearch(self.dataToSort)
